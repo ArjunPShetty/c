def1c54496d0deaf227fe555f6a04866545f3d50
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2026-02-08 17:23:26 +0530

    added the check_even_odd.c files

---
+++ check_even_odd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+int n;
+printf("Enter the number : ");
+scanf("%d",&n);
+
+if(n%2==0)
+{
+    printf("%d is even no",n);
+    
+}
+else
+{
+    printf("%d is odd no",n);
+}
+
+return 0;
+}
